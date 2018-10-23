59e57982292d53b02885fc3b06ab740297926659
nvaldebenito <nvaldebenito@inti.dgf.uchile.cl>
2018-10-23 15:00:10 -0300

    add click buttons for save configuration

--- process.py
+++ process.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import os 
 from selenium import webdriver
 
+#open dataframe DOI
 df = pd.read_csv("/home/nvaldebenito/Documentos/20_articles_process/cr2_articles.csv", sep='\t', encoding='utf-8')
 
 DOI = df["Copy for Cites DOI"]
@@ -16,6 +17,7 @@ DOI = df["Copy for Cites DOI"]
 DOI_SCOPUS=[]
 DOI_WEBOFS=[]
 
+#write string
 for i in range(0,len(DOI)):
 	DOI_SCOPUS.append(str(" DOI(")+str(DOI[i])+str(")"))
 
@@ -38,29 +40,38 @@ button.send_keys(DOI_WEBOFS)
 enter = driver.find_element_by_id('search-button')
 enter.click()
 
+#npub  = len(driver.find_elements_by_xpath('/summary.do?product=WOS&amp;doc=1&amp;qid=21&amp;SID=7CWt7eMDxKOrXhHC3cS&amp;search_mode=AdvancedSearch&amp;update_back2search_link_param=yes'))
+#print(npub)
+
 #click in number of publications found
 enter = driver.find_element_by_id('set_4_div')
 enter.click()
 
 #enter = driver.find_element_by_class_name('select2-selection__arrow')
 #enter.click()
+
 #save in other files format 
 enter = driver.find_element_by_id('select2-saveToMenu-container')
 enter.click()
 
-#
+#from number
 enter = driver.find_element_by_id('markFrom')
 enter.send_keys(1)
 
+#to number
 enter = driver.find_element_by_id('markTo')
-npub  = driver.find_element_by_id('hitCount.top')
-enter.send_keys(npub)
+enter.send_keys(243)
 
-enter = driver.find_element_by_id('select2-bib_fields-result-qq0h-HIGHLY_CITED HOT_PAPER OPEN_ACCESS PMID USAGEIND AUTHORSIDENTIFIERS ACCESSION_NUM FUNDING SUBJECT_CATEGORY JCR_CATEGORY LANG IDS PAGEC SABBR CITREFC ISSN PUBINFO KEYWORDS CITTIMES ADDRS CONFERENCE_SPONSORS DOCTYPE CITREF ABSTRACT CONFERENCE_INFO SOURCE TITLE AUTHORS  ')
+enter = driver.find_elements_by_xpath('//span[contains(@class, "select2") and contains(@class, "select2-container") and contains(@class, "select2-container--default") and contains(@class,"select2-container--below") and contains(@class,"select2-container--focus") and contains(@class,"select2-container--open")]')
+enter = driver.find_elements_by_xpath('//span[contains(@class,"select2-selection") and contains(@class,"select2-selection--single")]')
+enter = driver.find_element_by_xpath('//option[contains(@value,"HIGHLY_CITED HOT_PAPER OPEN_ACCESS PMID USAGEIND AUTHORSIDENTIFIERS ACCESSION_NUM FUNDING SUBJECT_CATEGORY JCR_CATEGORY LANG IDS PAGEC SABBR CITREFC ISSN PUBINFO KEYWORDS CITTIMES ADDRS CONFERENCE_SPONSORS DOCTYPE CITREF ABSTRACT CONFERENCE_INFO SOURCE TITLE AUTHORS  ")]')
+#enter = driver.find_element_by_id('select2-bib_fields-result-k0wy-HIGHLY_CITED HOT_PAPER OPEN_ACCESS PMID USAGEIND AUTHORSIDENTIFIERS ACCESSION_NUM FUNDING SUBJECT_CATEGORY JCR_CATEGORY LANG IDS PAGEC SABBR CITREFC ISSN PUBINFO KEYWORDS CITTIMES ADDRS CONFERENCE_SPONSORS DOCTYPE CITREF ABSTRACT CONFERENCE_INFO SOURCE TITLE AUTHORS  ')
 enter.click()
 
-enter = driver.find_element_by_id('select2-saveOptions-result-qs3k-tabWinUTF8')
-enter.click()
+#enter = driver.find_element_by_xpath('//span[contains(@class,"select2-selection") and contains(@class,"select2-selection--single")]')
 
-enter = driver.find_element_by_id('select2-saveToMenu-result-veek-other')
-enter.click()
+#enter = driver.find_element_by_id('select2-saveOptions-result-qs3k-tabWinUTF8')
+#enter.click()
+
+#enter = driver.find_element_by_id('select2-saveToMenu-result-veek-other')
+#enter.click()
