8c24a90739bbea4f86416ce35c354252bc17b173
nvaldebenito <nvaldebenito@inti.dgf.uchile.cl>
2018-10-23 12:24:02 -0300

    add click buttons for saving csv with data

--- process.py
+++ process.py
@@ -29,5 +29,38 @@ print(DOI_WEBOFS)
 
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
 driver.get("http://apps.webofknowledge.com/WOS_AdvancedSearch_input.do?SID=7CWt7eMDxKOrXhHC3cS&product=WOS&search_mode=AdvancedSearch")
+
+#add DOI
 button = driver.find_element_by_id('value(input1)')
 button.send_keys(DOI_WEBOFS)
+
+#start to search
+enter = driver.find_element_by_id('search-button')
+enter.click()
+
+#click in number of publications found
+enter = driver.find_element_by_id('set_4_div')
+enter.click()
+
+#enter = driver.find_element_by_class_name('select2-selection__arrow')
+#enter.click()
+#save in other files format 
+enter = driver.find_element_by_id('select2-saveToMenu-container')
+enter.click()
+
+#
+enter = driver.find_element_by_id('markFrom')
+enter.send_keys(1)
+
+enter = driver.find_element_by_id('markTo')
+npub  = driver.find_element_by_id('hitCount.top')
+enter.send_keys(npub)
+
+enter = driver.find_element_by_id('select2-bib_fields-result-qq0h-HIGHLY_CITED HOT_PAPER OPEN_ACCESS PMID USAGEIND AUTHORSIDENTIFIERS ACCESSION_NUM FUNDING SUBJECT_CATEGORY JCR_CATEGORY LANG IDS PAGEC SABBR CITREFC ISSN PUBINFO KEYWORDS CITTIMES ADDRS CONFERENCE_SPONSORS DOCTYPE CITREF ABSTRACT CONFERENCE_INFO SOURCE TITLE AUTHORS  ')
+enter.click()
+
+enter = driver.find_element_by_id('select2-saveOptions-result-qs3k-tabWinUTF8')
+enter.click()
+
+enter = driver.find_element_by_id('select2-saveToMenu-result-veek-other')
+enter.click()
